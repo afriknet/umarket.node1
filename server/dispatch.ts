da75ee9368afc6a04dd3ccf64b679e8d736f2029
seyaobey <seyaobey@gmail.com>
2016-06-06 16:59:16 +0300

    config

--- server/dispatch.ts
+++ server/dispatch.ts
@@ -1,2 +1,25 @@
 ﻿
 
+/// <reference path="../typings/express/express.d.ts" />
+
+
+import Express = require('express');
+
+var Schema = require('schema-client');
+var client = new Schema.Client('afriknetmarket', 'WRvloJ7OlLsNCAjPFfp1wJcRwyNU5pQ2');
+
+export function process(req: Express.Request, res: Express.Response) {
+
+    client.get('/products', { active: true }).then(data => {
+
+        res.send("hello world!");
+
+    }).catch(err=> {
+
+        res.send(err);
+
+    });
+
+
+
+}
