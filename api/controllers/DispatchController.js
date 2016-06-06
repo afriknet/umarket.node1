78e883c1739607a134f8c335f1bd68b050ef2f83
seyaobey <seyaobey@gmail.com>
2016-06-06 17:28:32 +0300

    dispatch

--- api/controllers/DispatchController.js
+++ api/controllers/DispatchController.js
@@ -1,11 +1,18 @@
 /**
- * DispatchController
+ * DispatcherController
  *
- * @description :: Server-side logic for managing dispatches
+ * @description :: Server-side logic for managing dispatchers
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var root = require('root-path');
+var dispatcher = require(root('/server/dispatch'));
+
 module.exports = {
-	
+    
+    dispatch: function (req, res) {
+        dispatcher.process(req, res);
+    }
+
 };
 
